19ddf36c718e2e941506059d041fc4ee2db4132e
Hpalling <105853474+Hpalling@users.noreply.github.com>
2022-06-01 11:40:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW
+{
+    class Program
+    {
+        public static string Username { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is ypur name?");
+            string UserName;
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + UserName);
+
+            Console.Read();
+        }
+    }
+}
+
